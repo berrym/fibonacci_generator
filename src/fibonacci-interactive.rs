cd7dee849a428edece3dd6c6e9441471ba569945
Michael Berry <trismegustis@gmail.com>
2019-11-29 00:37:20 -0500

    Ran rustfmt on source files.

--- src/fibonacci-interactive.rs
+++ src/fibonacci-interactive.rs
@@ -16,46 +16,49 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Print directly to stdout without a newline, flush stdout
         print!("\nEnter a positive integer from 1 to 184: ");
-        io::stdout().flush()
-            .unwrap();
+        io::stdout().flush().unwrap();
 
         // Read a line of user input
-        io::stdin().read_line(&mut input)
+        io::stdin()
+            .read_line(&mut input)
             .expect("Failed to read line");
 
         // If user input is quit, then break program loop
         if input.trim() == "quit" || input.trim() == "exit" {
-            return Ok(println!("Goodbye!"))
+            return Ok(println!("Goodbye!"));
         }
 
         // Parse input into a number
         let input: i64 = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-		println!("Invalid input!");
-		continue
-	    },
+                println!("Invalid input!");
+                continue;
+            }
         };
 
         let v = match fib::fibonacci_to_nth(input) {
-	    Ok(vec) => vec,
-	    Err(err) => {
-		println!("{}", err);
-		continue
-	    },
-	};
+            Ok(vec) => vec,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
+        };
 
         for (count, i) in v.iter().enumerate() {
             println!("{}: {}", count, i.to_formatted_string(&Locale::en));
         }
 
         let nth = fib::nth_fibonacci(input)?;
-	match input {
-	    1 => println!("The 1'st Fibonacci number is {}", nth),
-	    2 => println!("The 2'nd Fibonacci number is {}", nth),
-	    3 => println!("The 3'rd Fibonacci number is {}", nth),
-	    _ => println!("The {}'th Fibonacci number is {}",
-			  input, nth.to_formatted_string(&Locale::en)),
-	}
+        match input {
+            1 => println!("The 1'st Fibonacci number is {}", nth),
+            2 => println!("The 2'nd Fibonacci number is {}", nth),
+            3 => println!("The 3'rd Fibonacci number is {}", nth),
+            _ => println!(
+                "The {}'th Fibonacci number is {}",
+                input,
+                nth.to_formatted_string(&Locale::en)
+            ),
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 pub mod fib {
     fn validate_input(nth: i64) -> Result<bool, String> {
         if nth > 0 && nth < 185 {
-	    Ok(true)
+            Ok(true)
         } else {
             Err("Invalid input!".to_string())
-	}
+        }
     }
 
     /// Return result of a + b = n
     #[allow(dead_code)]
     pub fn nth_fibonacci(nth: i64) -> Result<u128, String> {
-	validate_input(nth)?;
+        validate_input(nth)?;
 
         let mut a: u128 = 1;
         let mut b: u128 = 1;
@@ -30,7 +30,7 @@ pub mod fib {
     /// Return a vector of Fibonacci numbers up to n'th number
     #[allow(dead_code)]
     pub fn fibonacci_to_nth(nth: i64) -> Result<Vec<u128>, String> {
-	validate_input(nth)?;
+        validate_input(nth)?;
 
         let mut v: Vec<u128> = Vec::new();
         let mut a: u128 = 1;
@@ -38,7 +38,7 @@ pub mod fib {
 
         v.push(b);
 
-	let limit = nth + 1;
+        let limit = nth + 1;
         for number in 1..limit {
             let c: u128 = a + b;
             a = b;
@@ -59,14 +59,14 @@ mod tests {
 
     #[test]
     fn test_nth_fibonacci() -> Result<(), String> {
-	assert_eq!(fib::nth_fibonacci(5), Ok(13));
-	Ok(())
+        assert_eq!(fib::nth_fibonacci(5), Ok(13));
+        Ok(())
     }
 
     #[test]
     fn test_fibonacci_to_nth() -> Result<(), String> {
-	let v = vec![1, 2, 3, 5, 8, 13];
-	assert_eq!(fib::fibonacci_to_nth(5), Ok(v));
-	Ok(())
+        let v = vec![1, 2, 3, 5, 8, 13];
+        assert_eq!(fib::fibonacci_to_nth(5), Ok(v));
+        Ok(())
     }
 }
